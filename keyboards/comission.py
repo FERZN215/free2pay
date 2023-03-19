bb2c8a5a8accf1268fa0cbf96beac4ef8e88e5e6
FERZ <arFReZa@yandex.ru>
2023-03-19 20:32:59 +0300

    clean 4 u

--- keyboards/comission.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-yes = InlineKeyboardButton('Да', callback_data='comission_yes')
-no = InlineKeyboardButton('Нет', callback_data='comission_no')
-
-comission_kb = InlineKeyboardMarkup().row(yes, no)
--- keyboards/license_agreement.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
-license_agreement = InlineKeyboardButton("Лицензионное соглашение", url='https://docs.google.com/document/d/1z4DwPFGdHvhsVY0ym7ECTy7cuwxb-Z2W')
-yes = InlineKeyboardButton("Принимаю", callback_data="license_yes")
-no = InlineKeyboardButton("Отклоняю", callback_data="license_no")
-license_agreement_kb = InlineKeyboardMarkup().add(license_agreement).row(yes, no)
