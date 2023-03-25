460fb7664ff4ee9129f3bf57360c7fa17a1018fc
Fenix888YT <fenixroma20015@gmail.com>
2023-03-26 03:12:18 +0700

    annother clear

--- personal_area/reviews.py
+++
@@ -1,7 +0,0 @@
-from aiogram import types
-from pymongo.database import Database
-async def reviews_process(message: types.Message, db:Database):
-    user = db["users"].find_one({"telegram_id":message.chat.id})
-    reviews = user["reviews"]
-    for review in reviews:
-        print(review)
